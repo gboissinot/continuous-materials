b19bd63c7b153417f4b39b351d5b3d88ab251c6a
Gregory Boissinot <gregory.boissinot@gmail.com>
2014-04-29 23:55:16 +0200

    Add Null Repository Test

---
+++ Libs/LibMongoLatestVersionResolver/src/test/java/fr/synchrotron/soleil/ica/ci/lib/mongodb/latestversionrresolver/repository/NullArtifactRepositoryTest.java
@@ -0,0 +1,44 @@
+package fr.synchrotron.soleil.ica.ci.lib.mongodb.latestversionrresolver.repository;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author Gregory Boissinot
+ */
+public class NullArtifactRepositoryTest {
+
+    private static ArtifactRepository artifactRepository;
+
+    @BeforeClass
+    public static void setup() {
+        artifactRepository = new NullArtifactRepository();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullOrg() {
+        artifactRepository.getLatestVersion(null, "aName", "aType", "aStatus");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullName() {
+        artifactRepository.getLatestVersion("anOrg", null, "aType", "aStatus");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullType() {
+        artifactRepository.getLatestVersion("anOrg", "aName", null, "aStatus");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullStatus() {
+        artifactRepository.getLatestVersion("anOrg", "aName", "aType", null);
+    }
+
+    @Test
+    public void testNullResult() {
+        assertNull(artifactRepository.getLatestVersion("anOrg", "aName", "aType", "aStatus"));
+    }
+}
