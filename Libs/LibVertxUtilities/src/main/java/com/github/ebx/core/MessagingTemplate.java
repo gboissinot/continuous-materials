795f640c95a7f08178016d5d6a818f538f6c5a94
Gregory Boissinot <gregory.boissinot@gmail.com>
2014-06-13 12:44:50 +0200

    Initial import

---
+++ Libs/LibVertxUtilities/src/main/java/com/github/ebx/core/MessagingTemplate.java
@@ -0,0 +1,111 @@
+package com.github.ebx.core;
+
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+
+/**
+ * @author Gregory Boissinot
+ */
+public class MessagingTemplate {
+
+    public static InternalMessagingTemplate address(EventBus eventBus, String address) {
+        return new InternalMessagingTemplate(eventBus, address);
+    }
+
+    static class InternalMessagingTemplate {
+
+        private static final long DEFAULTVALUE_TIMOUT = 10l;
+
+        private static final String KEY_ACTION = "action";
+        private static final String KEY_CONTENT = "content";
+
+        private final String address;
+        private final EventBus eventBus;
+        private String action;
+        private Object content;
+        private long timeout;
+
+        public InternalMessagingTemplate(EventBus eventBus, String address) {
+            this.eventBus = eventBus;
+            this.address = address;
+        }
+
+        public InternalMessagingTemplate action(String action) {
+            this.action = action;
+            return this;
+        }
+
+        public InternalMessagingTemplate content(JsonObject content) {
+            this.content = content;
+            return this;
+        }
+
+        public InternalMessagingTemplate content(JsonArray content) {
+            this.content = content;
+            return this;
+        }
+
+        public InternalMessagingTemplate content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public InternalMessagingTemplate content(Number content) {
+            this.content = content;
+            return this;
+        }
+
+        public InternalMessagingTemplate content(Boolean content) {
+            this.content = content;
+            return this;
+        }
+
+        public InternalMessagingTemplate content(byte[] content) {
+            this.content = content;
+            return this;
+        }
+
+
+        public InternalMessagingTemplate content(Character content) {
+            this.content = content;
+            return this;
+        }
+
+        public InternalMessagingTemplate content(Object content) {
+            //TODO Serialize Object into JSON
+            this.content = content;
+            return this;
+        }
+
+
+        public InternalMessagingTemplate timeout(long timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public InternalMessagingTemplate message(String action, Object content) {
+            this.action = action;
+            return content(content);
+        }
+
+        public <R> void send(Handler<AsyncResult<Message<R>>> replyHandler) {
+            if (timeout == 0) {
+                timeout = DEFAULTVALUE_TIMOUT;
+            }
+            eventBus.sendWithTimeout(address, getEventBusMessage(), timeout, replyHandler);
+        }
+
+        private JsonObject getEventBusMessage() {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.putString(KEY_ACTION, action);
+            jsonObject.putValue(KEY_CONTENT, content);
+            return jsonObject;
+        }
+    }
+
+
+}
