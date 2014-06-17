b228519227f40c5834d056b0865e7b143c8ca8cc
Gregory Boissinot <gregory.boissinot@gmail.com>
2014-06-17 10:27:49 +0200

    Fix status code

--- Libs/LibVertxUtilities/src/main/java/fr/synchrotron/soleil/ica/proxy/utilities/HttpClientProxy.java
+++ Libs/LibVertxUtilities/src/main/java/fr/synchrotron/soleil/ica/proxy/utilities/HttpClientProxy.java
@@ -71,6 +71,8 @@ public class HttpClientProxy {
 
     public void sendClientResponse(final HttpServerRequest request, HttpClientResponse clientResponse) {
         request.response().headers().set(clientResponse.headers());
+        request.response().setStatusCode(clientResponse.statusCode());
+        request.response().setStatusMessage(clientResponse.statusMessage());
         fixWarningCookieDomain(request, clientResponse);
         clientResponse.dataHandler(new Handler<Buffer>() {
             public void handle(Buffer data) {
