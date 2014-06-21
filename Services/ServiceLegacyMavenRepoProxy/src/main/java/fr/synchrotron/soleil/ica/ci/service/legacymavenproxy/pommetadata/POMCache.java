9a3b1b4d7bec6e06cc685048379d6d49806b330f
Gregory Boissinot <gregory.boissinot@gmail.com>
2014-06-21 22:40:05 +0200

    Use SHA1 encryption mechanism from Maven 2.1.0

--- Services/ServiceLegacyMavenRepoProxy/src/main/java/fr/synchrotron/soleil/ica/ci/service/legacymavenproxy/pommetadata/POMCache.java
+++ Services/ServiceLegacyMavenRepoProxy/src/main/java/fr/synchrotron/soleil/ica/ci/service/legacymavenproxy/pommetadata/POMCache.java
@@ -4,7 +4,6 @@ import org.vertx.java.core.Vertx;
 import org.vertx.java.core.shareddata.ConcurrentSharedMap;
 import org.vertx.java.core.shareddata.SharedData;
 
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -19,10 +18,18 @@ public class POMCache {
     private final ConcurrentSharedMap<String, String> pomContentMap;
     private final ConcurrentSharedMap<String, String> pomSha1Map;
 
+    private MessageDigest digester;
+
     public POMCache(Vertx vertx) {
         final SharedData sharedData = vertx.sharedData();
         pomContentMap = sharedData.getMap(KEY_CACHE_POM_CONTENT);
         pomSha1Map = sharedData.getMap(KEY_CACHE_POM_SHA1);
+
+        try {
+            digester = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getSha1(String pomSha1Path) {
@@ -31,21 +38,39 @@ public class POMCache {
 
     public void putPomContent(String pomPath, String pomContent) {
 
+        //--POM CONTENT
         pomContentMap.put(pomPath, pomContent);
 
+        //--SHA1
         final String sha1Path = pomPath + ".sha1";
-
-        try {
-            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
-            crypt.reset();
-            crypt.update(pomContent.getBytes("UTF-8"));
-            pomSha1Map.put(sha1Path, String.valueOf(crypt.digest()));
-
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+        digester.reset();
+        final byte[] bytes = pomContent.getBytes();
+        digester.update(bytes, 0, bytes.length);
+        final String sha1 = encode(digester.digest());
+        pomSha1Map.put(sha1Path, sha1);
 
     }
+
+
+    private String encode(byte[] binaryData) {
+        if (binaryData.length != 16 && binaryData.length != 20) {
+            int bitLength = binaryData.length * 8;
+            throw new IllegalArgumentException("Unrecognised length for binary data: " + bitLength + " bits");
+        }
+
+        String retValue = "";
+
+        for (int i = 0; i < binaryData.length; i++) {
+            String t = Integer.toHexString(binaryData[i] & 0xff);
+
+            if (t.length() == 1) {
+                retValue += ("0" + t);
+            } else {
+                retValue += t;
+            }
+        }
+
+        return retValue.trim();
+    }
+
 }
