3b697a05ac31439966e09ccb550b8a991b8f4785
Gregory Boissinot <gregory.boissinot@gmail.com>
2014-06-24 12:01:33 +0200

    Change toString()

--- Services/ServiceMultiRepoProxy/src/main/java/fr/synchrotron/soleil/ica/ci/service/multirepoproxy/RepositoryObject.java
+++ Services/ServiceMultiRepoProxy/src/main/java/fr/synchrotron/soleil/ica/ci/service/multirepoproxy/RepositoryObject.java
@@ -29,7 +29,7 @@ public class RepositoryObject {
 
     @Override
     public String toString() {
-        return "RepositoryObject{" +
+        return "{" +
                 "host='" + host + '\'' +
                 ", port=" + port +
                 ", uri='" + uri + '\'' +
